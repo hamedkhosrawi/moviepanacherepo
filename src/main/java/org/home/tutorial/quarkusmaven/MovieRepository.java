95a5a914813d18af11a55aee50580280e9875cee
Hamed khosrawi <khosrawi.hame2@gmail.com>
2023-04-23 09:20:07 +0330

    Update MovieRepository.java

--- src/main/java/org/home/tutorial/quarkusmaven/MovieRepository.java
+++ src/main/java/org/home/tutorial/quarkusmaven/MovieRepository.java
@@ -8,12 +8,9 @@ import java.util.List;
 public class MovieRepository implements PanacheRepository<Movie> {
     public List<Movie> findByCountry(String country){
 
-<<<<<<< HEAD
+
         return list("SLEECT m FROM Movie m WHERE m.country =?1 ORDER BY " +
         "DESC", country);
-=======
-        return list("SELECT m FROM Movie m WHERE m.country = ?1 ORDER BY id" +
-                " DESC", country);
->>>>>>> f251195 (microProfile Health Added.)
+
     }
 }
