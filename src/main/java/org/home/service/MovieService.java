f2363c120d5869430612c2720d877b1a526a95b9
hamed khosrawi <khosrawi.hame2@gmail.com>
2023-04-28 21:12:22 +0330

    some properties Added.

--- src/main/java/org/home/service/MovieService.java
+++ src/main/java/org/home/service/MovieService.java
@@ -2,9 +2,12 @@ package org.home.service;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.home.entity.Movie;
+import org.home.entity.MovieRepository;
 import org.home.exception.MovieException;
 
+import javax.inject.Inject;
 import javax.inject.Singleton;
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,10 +17,12 @@ public class MovieService {
     @ConfigProperty(name="exception.message.movie.title")
     public String MOVIE_NULL;
 
-
+    @Inject
+    MovieRepository movieRepository;
 
     private List<Movie> movieList = new ArrayList<>();
 
+    @Transactional
     public List<Movie> createMovie(Movie movie){
         if (movie == null){
             throw new MovieException(MOVIE_NULL);
